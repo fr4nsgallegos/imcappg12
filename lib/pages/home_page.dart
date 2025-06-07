0776da9942efbcea4332c64fec301f2435a97476
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-06 21:44:28 -0500

    change color and opacity

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,27 +7,86 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double valor = 8.5;
+  double red = 0, green = 0, blue = 0, opacity = 1.0;
+  bool? isChecked = false;
+  double fontSize = 15;
+
+  Widget buildSliderRow(
+    double variable,
+    String title,
+    String tipo,
+    Color color,
+    Function(double) onChangedFunction,
+  ) {
+    //TIPO c-color , t-tamaño, o-opacidad
+    return Row(
+      children: [
+        Text(title),
+        Expanded(
+          child: Slider(
+            max:
+                tipo == 'c'
+                    ? 255
+                    : tipo == 't'
+                    ? 30
+                    : 1,
+            min:
+                tipo == 'c'
+                    ? 0
+                    : tipo == 't'
+                    ? 5
+                    : 0,
+            activeColor: color,
+            inactiveColor: Colors.grey,
+            value: variable,
+            thumbColor: color,
+            onChanged: (value) {
+              onChangedFunction(value);
+              setState(() {});
+            },
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("IMC APP")),
-      body: Column(
-        children: [
-          Slider(
-            max: 10,
-            min: 7,
-            activeColor: Colors.red,
-            inactiveColor: Colors.amber,
-            value: valor,
-            thumbColor: Colors.green,
-            onChanged: (value) {
-              valor = value;
-              print(value);
-              setState(() {});
-            },
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ",
+              style: TextStyle(
+                fontSize: fontSize,
+                color: Color.fromRGBO(
+                  red.toInt(),
+                  green.toInt(),
+                  blue.toInt(),
+                  opacity,
+                ),
+              ),
+            ),
+            buildSliderRow(fontSize, "Tamaño", "t", Colors.cyan, (val) {
+              fontSize = val;
+            }),
+            buildSliderRow(red, "Red", "c", Colors.red, (val) {
+              red = val;
+            }),
+            buildSliderRow(green, "Green", "c", Colors.green, (val) {
+              green = val;
+            }),
+            buildSliderRow(blue, "Blue", "c", Colors.blue, (val) {
+              blue = val;
+            }),
+            buildSliderRow(opacity, "Opacity", "o", Colors.black, (val) {
+              opacity = val;
+            }),
+          ],
+        ),
       ),
     );
   }
