aeab7022293849f585f1129b5baa4a7063278178
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-06 20:50:24 -0500

    slider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double valor = 8.5;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("IMC APP")),
+      body: Column(
+        children: [
+          Slider(
+            max: 10,
+            min: 7,
+            activeColor: Colors.red,
+            inactiveColor: Colors.amber,
+            value: valor,
+            thumbColor: Colors.green,
+            onChanged: (value) {
+              valor = value;
+              print(value);
+              setState(() {});
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
